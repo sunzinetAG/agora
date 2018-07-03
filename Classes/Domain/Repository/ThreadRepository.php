d4375309dcb3f93d97f8883560608245a5909dc1
bresserbj <bjoern.bresser@sunzinet.com>
2018-07-03 10:53:45 +0200

    [HOTFIX] Add a missing braclet due to merge

--- Classes/Domain/Repository/ThreadRepository.php
+++ Classes/Domain/Repository/ThreadRepository.php
@@ -25,19 +25,21 @@ use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Persistence\QueryInterface;
 use AgoraTeam\Agora\Domain\Model\DemandInterface;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * The repository for Threads
  */
-class ThreadRepository extends \AgoraTeam\Agora\Domain\Repository\AbstractDemandedRepository	
-	/**
-	 * ForumRepository
-	 *
-	 * @var \AgoraTeam\Agora\Domain\Repository\ForumRepository
-	 * @inject
-	 */
-	protected $forumRepository;
+class ThreadRepository extends \AgoraTeam\Agora\Domain\Repository\AbstractDemandedRepository
+{
+
+    /**
+     * ForumRepository
+     *
+     * @var \AgoraTeam\Agora\Domain\Repository\ForumRepository
+     * @inject
+     */
+    protected $forumRepository;
+
     /**
      * Find threads by forum
      *
@@ -97,7 +99,7 @@ class ThreadRepository extends \AgoraTeam\Agora\Domain\Repository\AbstractDemand
 
         return $result;
     }
-    
+
     /**
      * Returns an array of constraints created from a given demand object.
      *
@@ -110,31 +112,31 @@ class ThreadRepository extends \AgoraTeam\Agora\Domain\Repository\AbstractDemand
      */
     protected function createConstraintsFromDemand(QueryInterface $query, DemandInterface $demand)
     {
-    	/** @var ForumDeman $demand */
-    	$constraints = [];
-    
-    	// storage page
-    	if ($demand->getStoragePage() != 0) {
-    		$pidList = GeneralUtility::intExplode(',', $demand->getStoragePage(), true);
-    		$constraints['pid'] = $query->in('pid', $pidList);
-    	}
-    	 
-    	// Search
-    	$searchConstraints = $this->getSearchConstraints($query, $demand);
-    	if (!empty($searchConstraints)) {
-    		$constraints['search'] = $query->logicalAnd($searchConstraints);
-    	}
-    	 
-    	// Clean not used constraints
-    	foreach ($constraints as $key => $value) {
-    		if (is_null($value)) {
-    			unset($constraints[$key]);
-    		}
-    	}
-    
-    	return $constraints;
+        /** @var ForumDeman $demand */
+        $constraints = [];
+
+        // storage page
+        if ($demand->getStoragePage() != 0) {
+            $pidList = GeneralUtility::intExplode(',', $demand->getStoragePage(), true);
+            $constraints['pid'] = $query->in('pid', $pidList);
+        }
+
+        // Search
+        $searchConstraints = $this->getSearchConstraints($query, $demand);
+        if (!empty($searchConstraints)) {
+            $constraints['search'] = $query->logicalAnd($searchConstraints);
+        }
+
+        // Clean not used constraints
+        foreach ($constraints as $key => $value) {
+            if (is_null($value)) {
+                unset($constraints[$key]);
+            }
+        }
+
+        return $constraints;
     }
-    
+
     /**
      * Returns an array of orderings created from a given demand object.
      *
@@ -143,35 +145,34 @@ class ThreadRepository extends \AgoraTeam\Agora\Domain\Repository\AbstractDemand
      */
     protected function createOrderingsFromDemand(DemandInterface $demand)
     {
-    	 
-    	$orderings = [];
-    	$orderField = $demand->getSearch()->getOrder();
-    	 
-    	if (!isset($orderField)) {
-    		$orderField = $demand->getOrder();
-    	}
-    	 
-    	$orderList = GeneralUtility::trimExplode(',', $orderField, true);
-    	 
-    	if (!empty($orderList)) {
-    		// go through every order statement
-    		foreach ($orderList as $orderItem) {
-    			list($orderField, $ascDesc) = GeneralUtility::trimExplode(' ', $orderItem, true);
-    			// count == 1 means that no direction is given
-    			if ($ascDesc) {
-    				$orderings[$orderField] = ((strtolower($ascDesc) == 'desc') ?
-    						QueryInterface::ORDER_DESCENDING :
-    						QueryInterface::ORDER_ASCENDING);
-    			} else {
-    				$orderings[$orderField] = QueryInterface::ORDER_ASCENDING;
-    			}
-    		}
-    	}
-    
-    	return $orderings;
+
+        $orderings = [];
+        $orderField = $demand->getSearch()->getOrder();
+
+        if (!isset($orderField)) {
+            $orderField = $demand->getOrder();
+        }
+
+        $orderList = GeneralUtility::trimExplode(',', $orderField, true);
+
+        if (!empty($orderList)) {
+            // go through every order statement
+            foreach ($orderList as $orderItem) {
+                list($orderField, $ascDesc) = GeneralUtility::trimExplode(' ', $orderItem, true);
+                // count == 1 means that no direction is given
+                if ($ascDesc) {
+                    $orderings[$orderField] = ((strtolower($ascDesc) == 'desc') ?
+                        QueryInterface::ORDER_DESCENDING :
+                        QueryInterface::ORDER_ASCENDING);
+                } else {
+                    $orderings[$orderField] = QueryInterface::ORDER_ASCENDING;
+                }
+            }
+        }
+
+        return $orderings;
     }
-    
-    
+
     /**
      * Get the search constraints
      *
@@ -182,59 +183,60 @@ class ThreadRepository extends \AgoraTeam\Agora\Domain\Repository\AbstractDemand
      */
     protected function getSearchConstraints(QueryInterface $query, DemandInterface $demand)
     {
-    	$constraints = [];
-    	$openUserForums = $this->forumRepository->findAccessibleUserForums();
-    	 
-    	if ($demand->getSearch() === null) {
-    		return $constraints;
-    	}
-    
-    	/* @var $searchObject \AgoraTeam\Agora\Domain\Model\Dto\Search */
-    	$searchObject = $demand->getSearch();
-    	$searchSubject = $searchObject->getSword();
-    	$searchForums = $searchObject->getThemes();
- 
-    	if (!empty($searchForums)) {
-    		$constraints[] = $query->in('forum', $searchForums);
-    	}
-    	
-    	
-    	if (!empty($searchSubject)) {
-    		$searchConstraints = [];
-    		$searchRadius = $searchObject->getRadius();
-    		$searchFields = array(0 => 'title');
-    		 
-    		if (count($searchFields) === 0) {
-    			throw new \UnexpectedValueException('No search fields defined', 1318497755);
-    		}
-    
-    		$searchSubjectSplitted = GeneralUtility::trimExplode(' ', $searchSubject, true);
-    		if ($searchObject->isSplitSubjectWords() && count($searchSubjectSplitted) > 1) {
-    			foreach ($searchFields as $field) {
-    				$subConstraints = [];
-    				foreach ($searchSubjectSplitted as $searchSubjectSplittedPart) {
-    					$subConstraints[] = $query->like($field, '%' . $GLOBALS['TYPO3_DB']->escapeStrForLike($searchSubjectSplittedPart, '') . '%');
-    				}
-    				$searchConstraints[] = $query->logicalAnd($subConstraints);
-    			}
-    
-    			if (count($searchConstraints)) {
-    				$constraints[] = $query->logicalOr($searchConstraints);
-    			}
-    		} else {
-    			foreach ($searchFields as $field) {
-    				if (!empty($searchSubject)) {
-    					$searchConstraints[] = $query->like($field, '%' . $GLOBALS['TYPO3_DB']->escapeStrForLike($searchSubject, '') . '%');
-    				}
-    			}
-    
-    			if (count($searchConstraints)) {
-    				$constraints[] = $query->logicalOr($searchConstraints);
-    			}
-    		}
-    	}
-    
-    	return $constraints;
+        $constraints = [];
+        $openUserForums = $this->forumRepository->findAccessibleUserForums();
+
+        if ($demand->getSearch() === null) {
+            return $constraints;
+        }
+
+        /* @var $searchObject \AgoraTeam\Agora\Domain\Model\Dto\Search */
+        $searchObject = $demand->getSearch();
+        $searchSubject = $searchObject->getSword();
+        $searchForums = $searchObject->getThemes();
+
+        if (!empty($searchForums)) {
+            $constraints[] = $query->in('forum', $searchForums);
+        }
+
+        if (!empty($searchSubject)) {
+            $searchConstraints = [];
+            $searchRadius = $searchObject->getRadius();
+            $searchFields = array(0 => 'title');
+
+            if (count($searchFields) === 0) {
+                throw new \UnexpectedValueException('No search fields defined', 1318497755);
+            }
+
+            $searchSubjectSplitted = GeneralUtility::trimExplode(' ', $searchSubject, true);
+            if ($searchObject->isSplitSubjectWords() && count($searchSubjectSplitted) > 1) {
+                foreach ($searchFields as $field) {
+                    $subConstraints = [];
+                    foreach ($searchSubjectSplitted as $searchSubjectSplittedPart) {
+                        $subConstraints[] = $query->like($field,
+                            '%' . $GLOBALS['TYPO3_DB']->escapeStrForLike($searchSubjectSplittedPart, '') . '%');
+                    }
+                    $searchConstraints[] = $query->logicalAnd($subConstraints);
+                }
+
+                if (count($searchConstraints)) {
+                    $constraints[] = $query->logicalOr($searchConstraints);
+                }
+            } else {
+                foreach ($searchFields as $field) {
+                    if (!empty($searchSubject)) {
+                        $searchConstraints[] = $query->like($field,
+                            '%' . $GLOBALS['TYPO3_DB']->escapeStrForLike($searchSubject, '') . '%');
+                    }
+                }
+
+                if (count($searchConstraints)) {
+                    $constraints[] = $query->logicalOr($searchConstraints);
+                }
+            }
+        }
+
+        return $constraints;
     }
 
     /**
